446af03201207a096827f214d921c15e6ffe036d
jfCoronel <jfc@us.es>
2026-05-19 11:46:28 +0200

    Ahora si funciona el cambio a la versión 4.1 de Mathjax

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <footer>
       MathEditor v {__APP_VERSION__} {t.developedBy} <a href="http://jfc.us.es" target="_blank" rel="noopener">jfCoronel</a>
-      · {t.renderedWith}{' '}<a href="https://www.mathjax.org/" target="_blank" rel="noopener">MathJax</a>
+      · {t.renderedWith}{' '}<a href="https://www.mathjax.org/" target="_blank" rel="noopener">MathJax 4.1</a>
       · {t.editedWith}{' '}<a href="https://codemirror.net" target="_blank" rel="noopener">CodeMirror</a>
     </footer>
   );
